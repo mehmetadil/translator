401c527c3b2fc3ceb4a307cf8448937a039c0b43
MehmetAdil <adil.istikbal@bil.omu.edu.tr>
2017-10-07 13:40:13 +0300

    fixed untracked files

--- app/models/article.rb
+++
@@ -1,7 +0,0 @@
-class Article < ApplicationRecord
-    #validates :name, :user_id, presence: true
-    belongs_to :user
-    belongs_to :language
-    has_many :article_materials
-    accepts_nested_attributes_for :article_materials
-end
--- app/models/article_material.rb
+++
@@ -1,8 +0,0 @@
-class ArticleMaterial < ApplicationRecord
-    belongs_to :article
-    has_attached_file :material #, :storage => :database
-    #validates_attachment_content_type :attachment, :content_type => 'text/plain'
-    #validates_format_of :attachment_file_name, :with => %r{\.(docx|doc|pdf)$\z/}i
-    #validates_attachment_content_type :file, :content_type => %w(application/msword application/vnd.ms-office application/vnd.ms-excel application/vnd.openxmlformats-officedocument.spreadsheetml.sheet)
-    do_not_validate_attachment_file_type :material    
-end
