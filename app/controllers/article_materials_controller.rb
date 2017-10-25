3470f9f842953d32e357352f1e0b2227e86d9701
MehmetAdil <adil.istikbal@bil.omu.edu.tr>
2017-10-25 22:17:11 +0300

    refactoring yapıldı

--- app/controllers/article_materials_controller.rb
+++ app/controllers/article_materials_controller.rb
@@ -1,26 +1,17 @@
 class ArticleMaterialsController < ApplicationController
+  before_action :set_article_material, only: [:download_material]
+
   def show
     @article_materials = ArticleMaterial.where(article_id: params[:id])
   end
 
   def download_material
-    send_file download_url
+    send_file @article_material.material.path
   end
 
   private
 
-  def download_url
-    file_folder_id = params[:id]
-    file_name = ArticleMaterial.find(params[:id]).material_file_name
-    if file_folder_id.to_i < 10
-      file_folder_name = '00' + file_folder_id
-    elsif file_folder_id.to_i < 100
-      file_folder_name = '0' + file_folder_id
-    else
-      file_folder_name = file_folder_id
-    end 
-    "#{Rails.root}" + "/public/system/article_materials
-                       /materials/000/000/#{file_folder_name}
-                       /original/#{file_name}"
+  def set_article_material
+    @article_material = ArticleMaterial.find(params[:id])
   end
 end
--- app/controllers/version_tracker_materials_controller.rb
+++ app/controllers/version_tracker_materials_controller.rb
@@ -1,25 +1,17 @@
 class VersionTrackerMaterialsController < ApplicationController
+  before_action :set_version_tracker_material, only: [:download_material]
+
   def show
     @version_tracker_materials = VersionTrackerMaterial.where(version_tracker_id: params[:id])
   end
 
   def download_material
-    send_file download_url
+    send_file @version_tracker_material.material.path
   end
 
   private
 
-  def download_url
-    file_folder_id = params[:id]
-    file_name = VersionTrackerMaterial.find(params[:id]).material_file_name
-    if file_folder_id.to_i < 10
-      file_folder_name = '00' + file_folder_id
-    elsif file_folder_id.to_i < 100
-      file_folder_name = '0' + file_folder_id
-    else
-      file_folder_name = file_folder_id
-    end
-    "#{Rails.root}" + "/public/system/version_tracker_materials
-                       /materials/000/000/#{file_folder_name}/original/#{file_name}"
+  def set_version_tracker_material
+    @version_tracker_material = VersionTrackerMaterial.find(params[:id])
   end
 end
