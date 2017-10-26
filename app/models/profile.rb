0a5af1c07e06a599f133bbeb195412770e2961e6
MehmetAdil <adil.istikbal@bil.omu.edu.tr>
2017-10-26 20:05:55 +0300

    profile nesnesi user oluşturulduktan sonra oluşturuluyor

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,9 @@
 class Profile < ApplicationRecord
   belongs_to :user
+
+  def self.create_profile(user_id, first_name, last_name = '')
+    @profile = Profile.new(first_name: first_name, last_name: last_name,
+                           user_id: user_id)
+    @profile.save
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,9 @@ class User < ApplicationRecord
   has_many :offers, foreign_key: 'translator_id', primary_key: 'id', class_name: 'Offer'
   has_many :tasks, foreign_key: 'translator_id', primary_key: 'id', class_name: 'Task'
   has_many :translate_orders
+  after_commit :create_profile, on: :create
+
+  def create_profile
+    Profile.create_profile(id, email)
+  end
 end
