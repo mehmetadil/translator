37ce7dbc17651e305f30ab93f6be8ced93a70a00
MehmetAdil <adil.istikbal@bil.omu.edu.tr>
2017-10-26 23:01:42 +0300

    full_name fonksiyonu oluşturuldu, article index sayfası düzenlendi

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -8,4 +8,8 @@ class Profile < ApplicationRecord
                     },
                     default_url: ActionController::Base.helpers.asset_path('default_avatar.svg')
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
+  def full_name
+    [first_name, last_name].reject(&:empty?).join(' ')
+  end
 end
