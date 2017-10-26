eae69a0ffcda4f21e87dcb2579f8baa80308ff22
MehmetAdil <adil.istikbal@bil.omu.edu.tr>
2017-10-26 20:56:04 +0300

    profile oluşturma yöntemi değiştirildi

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,9 +1,3 @@
 class Profile < ApplicationRecord
   belongs_to :user
-
-  def self.create_profile(user_id, first_name, last_name = '')
-    @profile = Profile.new(first_name: first_name, last_name: last_name,
-                           user_id: user_id)
-    @profile.save
-  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,12 @@ class User < ApplicationRecord
   has_many :offers, foreign_key: 'translator_id', primary_key: 'id', class_name: 'Offer'
   has_many :tasks, foreign_key: 'translator_id', primary_key: 'id', class_name: 'Task'
   has_many :translate_orders
-  after_commit :create_profile, on: :create
+  after_create :build_profile
 
-  def create_profile
-    Profile.create_profile(id, email)
+  def build_profile
+    create_profile(
+      first_name: '',
+      last_name: ''
+    )
   end
 end
