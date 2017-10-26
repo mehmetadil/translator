ce13c8ff767ce46abc9a79ce6362d9d497caece8
MehmetAdil <adil.istikbal@bil.omu.edu.tr>
2017-10-26 22:17:10 +0300

    kullanıcılar için default avatar oluşturuldu

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -5,6 +5,7 @@ class Profile < ApplicationRecord
                       thumb:   '10x10#',
                       small:   '150x150>',
                       medium: '210x260#'
-                    }
+                    },
+                    default_url: ActionController::Base.helpers.asset_path('default_avatar.svg')
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
