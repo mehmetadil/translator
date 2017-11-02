6c6ec419939abe65ccb3d3b633276f7350555da0
MehmetAdil <adil.istikbal@bil.omu.edu.tr>
2017-11-02 14:43:57 +0300

    Closes #10. makale sahibi her bir hedef dil için tek bir çeviri isteğinde bulunabilir

--- app/models/translate_order.rb
+++ app/models/translate_order.rb
@@ -4,4 +4,5 @@ class TranslateOrder < ApplicationRecord
   belongs_to :target_language, foreign_key: 'target_language_id', class_name: 'Language'
   belongs_to :source_language, foreign_key: 'source_language_id', class_name: 'Language'
   has_many :offers, dependent: :destroy
+  validates_uniqueness_of :source_language_id, scope: :target_language_id
 end
